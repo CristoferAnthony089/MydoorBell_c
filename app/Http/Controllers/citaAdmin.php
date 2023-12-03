1e52d9139fb4334220b6c136ffb241bbd958e4f6
CristoferAnthony089 <cristo045millanperez@gmail.com>
2023-12-03 17:53:12 -0600

    correciones

--- app/Http/Controllers/citaAdmin.php
+++ app/Http/Controllers/citaAdmin.php
@@ -10,16 +10,15 @@ use Illuminate\Http\Request;
 class citaAdmin extends Controller
 {
 
-  public function index() {
+    public function index()
+    {
 
-    
-    $citas = DB::table('citas')
-    ->join('direcciones', 'citas.direcciones_id_dir', '=', 'direcciones.id_dir')  
-    ->select('citas.*', 'direcciones.*')
-    ->get();
-    
-return view('admin.cita', compact('citas')); 
-  
-  }
 
+        $citas = DB::table('citas')
+            ->join('direcciones', 'citas.direcciones_id_dir', '=', 'direcciones.id_dir')
+            ->select('citas.*', 'direcciones.*')
+            ->get();
+
+        return view('admin.cita', compact('citas'));
+    }
 }
